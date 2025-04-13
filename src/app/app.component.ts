5470f09d8bc508b1c003f9bdf8390df7a54c385d
ZV <wazolee@gmail.com>
2025-04-13 22:01:02 +0200

    end 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatMenuModule } from '@angular/material/menu';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   standalone: true,
@@ -11,10 +12,21 @@ import { HomeComponent } from './home/home.component';
     MatButtonModule,
     HeaderComponent,
     HomeComponent,
+    RouterOutlet
   ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+constructor(private readonly router:Router){}
+
+ngOnInit(): void {
+  this.router.events.subscribe((event) => {
+    console.log('router event:', event);
+  });
+}
+
+
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,12 @@
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { ApplicationConfig } from '@angular/core';
+import { provideRouter, withDebugTracing } from '@angular/router';
+import { ROUTES } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideAnimations(),
+    provideRouter(ROUTES, withDebugTracing()),
   ]
 };
 
